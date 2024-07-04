6440d8019da90ba6d19b215f7d96c2edaa5e3976
Arthur07Melo <aarthur00melo@gmail.com>
2024-07-04 19:50:34 -0300

    fix: new constructor for user using roles

--- microservices/book/src/main/java/bookify/ms/book/core/domain/User.java
+++ microservices/book/src/main/java/bookify/ms/book/core/domain/User.java
@@ -16,6 +16,13 @@ public class User {
     private String password;
     private List<Role> roles;
     
+    public User(String name, String email, String password, List<Role> roles) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
     public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
