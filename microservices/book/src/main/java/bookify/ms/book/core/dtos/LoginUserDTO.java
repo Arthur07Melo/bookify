b39d7488c669803a77b3155a720ccf0a726bab1c
Arthur07Melo <aarthur00melo@gmail.com>
2024-07-04 19:51:05 -0300

    chore: old DTOs deleted

--- microservices/book/src/main/java/bookify/ms/book/core/dtos/LoginUserDTO.java
+++
@@ -1,7 +0,0 @@
-package bookify.ms.book.core.dtos;
-
-public record LoginUserDTO(
-    String email,
-    String password
-) {
-}
--- microservices/book/src/main/java/bookify/ms/book/core/dtos/RegisterUserDTO.java
+++
@@ -1,8 +0,0 @@
-package bookify.ms.book.core.dtos;
-
-public record RegisterUserDTO(
-    String name,
-    String email,
-    String password
-) {
-}
