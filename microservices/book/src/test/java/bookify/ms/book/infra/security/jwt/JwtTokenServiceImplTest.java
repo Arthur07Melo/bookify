0623cea6654f889b35e672b6f02b1ef078d209cb
Arthur07Melo <aarthur00melo@gmail.com>
2024-07-08 15:39:21 -0300

    test: jwtTokenService implementation unit tests

---
+++ microservices/book/src/test/java/bookify/ms/book/infra/security/jwt/JwtTokenServiceImplTest.java
@@ -0,0 +1,39 @@
+package bookify.ms.book.infra.security.jwt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+public class JwtTokenServiceImplTest {
+    final String ISSUER = "microservice-book";
+    final Algorithm ALGORITHM = Algorithm.HMAC256("test_secret_key");
+    
+    JwtTokenServiceImpl jwtTokenService = new JwtTokenServiceImpl("test_secret_key");
+
+    @Test
+    public void testSuccessfullyGenerateToken(){
+        String token = jwtTokenService.generateToken("teste@teste.com");
+        var decodedToken = JWT.require(ALGORITHM)
+            .withIssuer(ISSUER)
+            .build()
+            .verify(token);
+        
+        assertEquals("teste@teste.com", decodedToken.getSubject());
+    } 
+
+    @Test
+    public void testSuccessfullyDecodeToken(){
+        String expectedSubject = "teste2@teste.com";
+        String encodedToken = JWT.create()
+        .withIssuer(ISSUER)
+        .withSubject(expectedSubject)
+        .sign(ALGORITHM);
+    
+        String subject = jwtTokenService.getSubjectFromToken(encodedToken);
+        assertEquals(expectedSubject, subject);
+    }
+
+}
