bc24ed09002b79b603be9cd5ddd86388ec51a9a1
Arthur07Melo <aarthur00melo@gmail.com>
2024-07-29 21:57:29 -0300

    feat: added controller for registering books

--- microservices/book/src/main/java/bookify/ms/book/infra/controllers/BookController.java
+++ microservices/book/src/main/java/bookify/ms/book/infra/controllers/BookController.java
@@ -1,15 +1,21 @@
 package bookify.ms.book.infra.controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import bookify.ms.book.core.domain.Book;
 import bookify.ms.book.core.dtos.request.RegisterBookDTO;
 import bookify.ms.book.core.usecases.RegisterBookUseCase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -23,10 +29,31 @@ public class BookController {
     }
 
     @PostMapping
-    public ResponseEntity<Book> postMethodName(@RequestBody RegisterBookDTO dto) {
+    // @RequestBody RegisterBookDTO dto
+    public ResponseEntity<Book> postBook(
+        @RequestParam("file") MultipartFile file,
+        @RequestParam("title") String title,
+        @RequestParam("price") float price,
+        @RequestParam("gender") String gender,
+        @RequestParam("author") String author
+        ) {
         log.info("Starting new book register");
-        var book = registerBookUseCase.execute(dto, null);
+        var dto = new RegisterBookDTO(title, price, gender, author);
+        File bookFile = convertMultipartFileToFile(file);
+        var book = registerBookUseCase.execute(dto, bookFile);
         return ResponseEntity.status(201).body(book);
     }
     
+
+    
+    private File convertMultipartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try {
+            Files.copy(file.getInputStream(), convertedFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return convertedFile;
+    }
+
 }
